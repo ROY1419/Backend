5c6ae3526ba90aaa1256854c407e20126161f7a7
Roy1419 <royshubham471@gmail.com>
2024-12-11 16:33:34 +0530

    this commit is for test/test.js file modification

--- Ms-Backend/test/test.js
+++ Ms-Backend/test/test.js
@@ -1,2 +1,23 @@
 import autocannon from "autocannon";
 
+const urls = ['http://localhost:3000', 'http://localhost:3000/stress-test']
+const duration = 30;
+
+urls.forEach(url => {
+    const instance = autocannon({
+        url,
+        duration,
+    }, (err, res) => {
+        try {
+            console.log(`URL: ${url}`);
+            console.log('Number of Request:', res.requests.total);
+            console.log('Duration(Second):', res.duration);
+        } catch (error) {
+            console.error('Error', err);
+        }
+    });
+    autocannon.track(instance, {renderProgressBar: false, renderResultsTable:false})
+})
+
+
+
